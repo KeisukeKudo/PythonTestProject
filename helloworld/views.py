9dec7d59262caca045189dc4f63cdcd673a1fa7f
KeisukeKudo <melchior.balthasar.casper326@gmail.com>
2017-11-14 16:02:35 +0900

    add css

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,13 +1,13 @@
-from django.http import HttpResponse
-from django.template import loader
+# from django.http import HttpResponse
+# from django.template import loader
+from django.shortcuts import render
 from .models import AddWord
 
 
 def index(request):
     data_list = AddWord.objects.all()
-    template = loader.get_template('helloworld/index.html')
     context = {
         'lists': data_list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'helloworld/index.html', context)
 
